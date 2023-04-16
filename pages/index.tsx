f3ba37071cd05b297aa1989fa6d1c805e2aa6f7b
김범식 <kbg7635@naver.com>
2023-04-17 03:05:59 +0900

    home화면 정적생성으로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,33 @@ import Head from "next/head";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ItemList from "./src/component/ItemList";
-import { Header } from "semantic-ui-react";
+import { Header, Divider } from "semantic-ui-react";
 import { Loader } from "semantic-ui-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+export default function Home({list}:any) {
 
-  //브라우저 환경에서 환경변수 꺼내는 방법
-  const API_URL:string = process.env.NEXT_PUBLIC_API_URL || '';
+  // 정적 페이지를 만들게되면 이모든게 필요 없어진다. 
+  // const [list, setList] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  function getData() {
-    axios.get(API_URL).then((res: any) => {
-      if (res.data) setList(res.data);
-      setIsLoading(false);
-    });
-  }
+  // //브라우저 환경에서 환경변수 꺼내는 방법
+  
+  // const API_URL:string = process.env.NEXT_PUBLIC_API_URL || '';
+  
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // function getData() {
+  //   axios.get(API_URL).then((res: any) => {
+  //     if (res.data) setList(res.data);
+  //     console.log("화장품 정보 >>> ",res.data)
+  //     setIsLoading(false);
+  //   });
+  // }
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
     <>
@@ -33,7 +38,7 @@ export default function Home() {
         <title>my next.js</title>
         <meta name="description" content="my-first-next.js"/>
       </Head>
-      {isLoading && (
+      {/* {isLoading && (
         <div style={{ padding: "300px 0" }}>
           <Loader inline="centered" active>
             Loading
@@ -55,7 +60,33 @@ export default function Home() {
           <hr />
           <ItemList list={list.slice(9)} />
         </>
-      )}
+      )} */}
+
+        <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          {list && <ItemList list={list.slice(0, 9)} />}
+
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          {list &&<ItemList list={list.slice(9)} />}
     </>
   );
+}
+
+//이제 이거 사용하면 정적페이지를 만드는것이라 로딩화면이 필요가 없다. 
+export async function getStaticProps(){
+  const apiUrl = process.env.apiUrl || ''; //node.js환경에서 불러오는거라 NEXT_PUBLIC 사용 안해도 됨 
+  const res = await axios.get(apiUrl);
+
+  return {
+    props:{
+      list:res.data,
+      name:process.env.name,
+    }
+  }
+
 }
